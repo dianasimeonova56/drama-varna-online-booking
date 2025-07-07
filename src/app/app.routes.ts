31ed45f06a62df4579ea4cb54ccb0c5938693eef
dianasim <diana.simeonova56@gmail.com>
2025-07-07 19:27:54 +0300

    modify route for home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/index';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     { path: '', component: Home },
+];
