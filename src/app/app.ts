fee447c7d72e2bd983dced73b6336aa6d04869bb
dianasim <diana.simeonova56@gmail.com>
2025-07-07 19:19:04 +0300

    add initial home landing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from "./shared/components/header/header";
-import { Footer } from "./shared/components/footer/footer";
+import { Header, Footer } from "./shared/components/index";
+import { HomeLanding } from "./features/home/home-landing/home-landing";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, HomeLanding],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/features/home/home-landing/home-landing.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-landing',
+  imports: [],
+  templateUrl: './home-landing.html',
+  styleUrl: './home-landing.css'
+})
+export class HomeLanding {
+
+}
