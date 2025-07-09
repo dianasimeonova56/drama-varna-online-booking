f8b03489e74f7f05c871a3c3a1d4f70a8144a2d6
dianasim <diana.simeonova56@gmail.com>
2025-07-09 13:43:32 +0300

    plays service added

---
+++ src/app/core/services/plays.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Play } from "../../models/play.model";
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class PlaysService {
+    private apiUrlForLatest = 'http://localhost:3000/api/get-latest-plays?limit={0}';
+    private apiUrl = 'http://localhost:3000/api/plays';
+
+    constructor(private httpClient:HttpClient) {}
+
+    getLatestPlays(limit: number = 1): Observable<Play[]> {
+        return this.httpClient.get<Play[]>(this.apiUrlForLatest.replace('{0}', limit.toString()) )
+    }
+
+    getPlays(): Observable<Play[]> {
+        return this.httpClient.get<Play[]>(this.apiUrl);
+    }
+}
