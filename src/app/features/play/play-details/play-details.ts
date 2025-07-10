2e4ae208d3601c4fcd9a60d87316a03c454da136
dianasim <diana.simeonova56@gmail.com>
2025-07-10 19:05:53 +0300

    play details loads data successfully

--- src/app/features/play/play-details/play-details.ts
+++ src/app/features/play/play-details/play-details.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { Play } from '../../../models';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { PlaysService } from '../../../core/services/plays.service';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-play-details',
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './play-details.html',
-  styleUrl: './play-details.css'
+  styleUrl: './play-details.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PlayDetailsComponent {
+  @Input() play$!: Observable<Play>;
 
+  //spinner?
+  constructor(private route: ActivatedRoute,
+    private playsService: PlaysService) {}
+
+  ngOnInit(): void {
+    const playId = this.route.snapshot.paramMap.get('playId');
+    if (playId) {
+      this.play$ = this.playsService.getPlay(playId);
+    }
+  }
 }
