1ea30d8c7ec76945dcdfe5d5f29bb6ae4f1bbb37
dianasim <diana.simeonova56@gmail.com>
2025-07-30 14:47:40 +0300

    fix routes for now

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,18 @@
 import { Routes } from '@angular/router';
-import { Home } from './features/home/index';
-import { PlaysComponent } from './features/play/plays-component/plays-component';
-import { PlayDetailsComponent } from './features/play/play-details/play-details';
 
 export const routes: Routes = [
-     { path: '', component: Home },
-     { path: 'plays', component: PlaysComponent },
-     { path: 'plays/:playId', component: PlayDetailsComponent}
+     {
+          path: '',
+          redirectTo: '/home',
+          pathMatch: 'full'
+     },
+     {
+          path: 'home',
+          loadComponent: () => import('./features/home/home/home').then(c => c.Home)
+     },
+     {
+          path: 'plays/:playId',
+          loadComponent: () => import('./features/play/play-details/play-details')
+               .then(m => m.PlayDetailsComponent)
+     }
 ];
