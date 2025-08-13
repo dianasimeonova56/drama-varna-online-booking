414bef6574026fa5acf8a4ea3317fb5d840b9825
dianasim <diana.simeonova56@gmail.com>
2025-08-13 15:27:13 +0300

    add swiperjs for home landing page, responsive design fro swiper

--- src/app/features/home/home-landing/home-landing.ts
+++ src/app/features/home/home-landing/home-landing.ts
@@ -1,17 +1,35 @@
-import { Component, computed, inject } from '@angular/core';
+import { AfterViewInit, Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { AuthService } from '../../../core/services';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home-landing',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './home-landing.html',
-  styleUrl: './home-landing.css'
+  styleUrl: './home-landing.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class HomeLanding {
-  protected authService = inject(AuthService);
-
-  readonly isLoggedIn = this.authService.isLoggedIn;
-  readonly currentUser = this.authService.currentUser;
-  protected role = computed(() => this.currentUser()?.role);
+export class HomeLanding   {
+  // swiper!: Swiper;
+  
+  // ngAfterViewInit() {
+  //   this.swiper = new Swiper('.swiper-container', {
+  //     loop: true,
+  //     autoplay: { delay: 3000 },
+  //     navigation: true,
+  //   });
+  // }
+  slides = [
+    {
+      src: 'images/dramavarna1.jpg',
+      title: 'Theater'
+    },
+    {
+      src: 'images/dramavarna2.jpg',
+      title: 'Theater',
+    }
+  ];
+  trackBySrc(index: number, slide: any): string {
+    return slide.src;
+  }
 }
