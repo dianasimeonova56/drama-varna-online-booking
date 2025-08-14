3b16bc3536a2b71c86d83d5ac46062760c27b37f
dianasim <diana.simeonova56@gmail.com>
2025-08-14 19:57:14 +0300

    user can now add rating only once

--- src/app/shared/components/star-rating-component/star-rating-component.ts
+++ src/app/shared/components/star-rating-component/star-rating-component.ts
@@ -2,34 +2,41 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FaIconComponent } from "@fortawesome/angular-fontawesome";
 import { faStar } from '@fortawesome/free-solid-svg-icons/faStar';
 import { PlaysService } from '../../../core/services/plays.service';
-import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-star-rating-component',
-  imports: [FaIconComponent],
+  imports: [FaIconComponent, CommonModule],
   templateUrl: './star-rating-component.html',
   styleUrl: './star-rating-component.css'
 })
 export class StarRatingComponent implements OnInit {
-  @Input() rating!: number;
+  @Input() rating!: number | null;
   @Input() playId!: string;
   @Output() ratingUpdated = new EventEmitter<number>();
   faStar = faStar;
   hasRated = false;
+  justRated = false;
+  triedToRateAgain = false;
 
   constructor(private playsService: PlaysService) { }
 
   ngOnInit() {
-    // Check localStorage to see if user has already rated this play
-    this.hasRated = !!localStorage.getItem(`rated-${this.playId}`);
+    this.playsService.getUserRating(this.playId).subscribe({
+      next: ({ hasRated }) => {
+        //TODO log message
+        this.hasRated = hasRated;
+      },
+      error: (err) => console.error(err)
+    })
   }
 
   setRating(value: number) {
-    
-    if (this.hasRated) return;
+    if (this.hasRated) {
+      this.triedToRateAgain = true;
+    };
 
     this.hasRated = true;
-    localStorage.setItem(`rated-${this.playId}`, 'true');
 
     this.playsService.addRating(this.playId, value)
       .subscribe({
@@ -37,11 +44,11 @@ export class StarRatingComponent implements OnInit {
           const newAverage = response.averageRating;
           this.rating = Number(newAverage.toFixed(2));
           this.ratingUpdated.emit(newAverage)
+          this.justRated = true;
         },
         error: err => {
           console.error(err)
-          this.hasRated = false; // unblock on error
-          localStorage.removeItem(`rated-${this.playId}`);
+          this.justRated = false;
         }
       })
   }
