af8841538b680cc8ed628f8194eae70d45f7534b
dianasim <diana.simeonova56@gmail.com>
2025-08-17 13:42:37 +0300

    leave only button to see booked plays in profile page (css later)

--- src/app/features/profile/user-profile/user-profile.ts
+++ src/app/features/profile/user-profile/user-profile.ts
@@ -1,45 +1,20 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { AuthService, TicketsService, PlaysService } from '../../../core/services';
-import { Booking, Play, Ticket } from '../../../models';
-import { BookingService } from '../../../core/services/booking.service';
+import { Component, inject, signal } from '@angular/core';
+import { AuthService } from '../../../core/services';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user-profile',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './user-profile.html',
   styleUrl: './user-profile.css'
 })
-export class UserProfile implements OnInit {
+export class UserProfile {
   authService = inject(AuthService);
-  ticketsService = inject(TicketsService);
-  bookingService = inject(BookingService);
-  playService = inject(PlaysService);
   isOpen = signal(true);
-
-
+  
   editing = false;
   user = { ...this.authService.currentUser() };
-  tickets = signal<Ticket[]>([])
-  bookings = signal<Booking[]>([]);
-  plays = signal<Play[]>([]);
 
-
-  ngOnInit(): void {
-    if (this.user?._id) {
-      this.ticketsService.getTickets(this.user._id).subscribe({
-        next: (t) => this.tickets.set(t),
-        error: (err) => console.error('Failed to load tickets', err)
-      });
-      this.bookingService.getBookings(this.user._id).subscribe({
-        next: (b) => this.bookings.set(b),
-        error: (err) => console.error('Failed to load bookings', err)
-      });
-    }
-  }
-
-  togglePlays() {
-    this.isOpen.update((isOpen) => !isOpen);
-  }
 
   toggleEdit() {
     this.editing = !this.editing;
