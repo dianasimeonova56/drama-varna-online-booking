dcc87f106e1dc1cf109a36ac9e6e59daecdf20d2
dianasim <diana.simeonova56@gmail.com>
2025-08-20 12:19:34 +0300

    add  angular materials spinenr & animate it along with booking item to appear smoothly

--- src/app/features/booking/booking-item/booking-item.ts
+++ src/app/features/booking/booking-item/booking-item.ts
@@ -1,26 +1,34 @@
-import { ChangeDetectorRef, Component, inject, Input } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, Input, OnInit } from '@angular/core';
 import { PopulatedBooking, Ticket } from '../../../models';
 import { PlayDateFormatPipe } from "../../../shared/pipes/playDateFormat.pipe";
 import { TicketComponent } from '../../ticket/ticket-component/ticket-component';
 import { TicketsService } from '../../../core/services';
-import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-booking-item',
-  imports: [PlayDateFormatPipe, TicketComponent, CommonModule],
+  imports: [PlayDateFormatPipe, TicketComponent, CommonModule, MatProgressSpinnerModule],
   templateUrl: './booking-item.html',
   styleUrl: './booking-item.css'
 })
-export class BookingItem {
+export class BookingItem implements OnInit{
   @Input() booking!: PopulatedBooking;
   tickets: Ticket[] | null = null;
   expanded = false;
+  loading = true;
 
   private ticketsService = inject(TicketsService);
 
   constructor(private cdr: ChangeDetectorRef) {
     //this.tickets$ = this.ticketsService.tickets$;
+  }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.loading = false;
+      this.cdr.detectChanges();
+    }, 1000);
   }
 
   toggleTickets() {
