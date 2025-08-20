a1acec93d5e8cc70587475d783a408948cd86521
dianasim <diana.simeonova56@gmail.com>
2025-08-20 15:16:52 +0300

    remove success stuff from error service

--- src/app/core/services/error.service.ts
+++ src/app/core/services/error.service.ts
@@ -6,18 +6,11 @@ import { Injectable, signal } from "@angular/core";
 
 export class ErrorService {
     private _error = signal<string | null>(null);
-    private _success = signal<string | null>(null);
 
     public error = this._error.asReadonly();
-    public success = this._success.asReadonly();
 
     setError(message: string) {
         this._error.set(message);
         setTimeout(() => this._error.set(null), 5000);
-    }
-
-    setSuccess(message:string) {
-        this._success.set(message);
-        setTimeout(() => this._success.set(null), 5000);
     }
 }
