6ced1a1ebfa0c4a9dd9f03068999229cb903d77c
dianasim <diana.simeonova56@gmail.com>
2025-08-20 13:02:38 +0300

    add config for angular aniamtions

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,15 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import { register as registerSwiperElements } from 'swiper/element-bundle'
-
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 registerSwiperElements();
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  ...appConfig,
+  providers: [
+    ...(appConfig.providers || []), 
+    provideAnimations()
+  ]
+})
+.catch((err) => console.error(err));
