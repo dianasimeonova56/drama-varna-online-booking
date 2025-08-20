431f2e932791d8c158535202e7136bb69c5ee615
dianasim <diana.simeonova56@gmail.com>
2025-08-20 13:26:43 +0300

    remove animation from confirmation bc it get overrriden by page transition

--- src/app/features/booking/confirmation/confirmation.ts
+++ src/app/features/booking/confirmation/confirmation.ts
@@ -1,22 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { animate, style, transition, trigger } from '@angular/animations';
 
 
 @Component({
   selector: 'app-confirmation',
   imports: [RouterModule],
   templateUrl: './confirmation.html',
-  styleUrl: './confirmation.css',
-  animations: [
-    trigger('fadeSlideIn', [
-      transition(':enter', [
-        style({ opacity: 0, transform: 'translateY(-20px)' }),
-        animate('600ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
-      ])
-    ])
-  ]
+  styleUrl: './confirmation.css'
 })
 export class Confirmation {
-  ngOnInit() {}
 }
