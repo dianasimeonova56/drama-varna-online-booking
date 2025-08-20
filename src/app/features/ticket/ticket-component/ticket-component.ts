7a8fc98bddf6be2cda71073e447724eb150c0465
dianasim <diana.simeonova56@gmail.com>
2025-08-20 12:20:05 +0300

    add  angular materials spinenr & animate it along with ticket  item to appear smoothly

--- src/app/features/ticket/ticket-component/ticket-component.ts
+++ src/app/features/ticket/ticket-component/ticket-component.ts
@@ -1,18 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectorRef, Component, Input } from '@angular/core';
 import { Ticket } from '../../../models';
 import { PlayDateFormatPipe } from "../../../shared/pipes/playDateFormat.pipe";
-
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-ticket-component',
-  imports: [PlayDateFormatPipe],
+  imports: [PlayDateFormatPipe, MatProgressSpinnerModule],
   templateUrl: './ticket-component.html',
   styleUrl: './ticket-component.css'
 })
 export class TicketComponent {
   @Input() ticket!: Ticket | null;
+  loading =  true;
 
+  constructor(private cdr: ChangeDetectorRef) {}
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.loading = false;
+      this.cdr.detectChanges();
+    }, 1000);
   }
 }
