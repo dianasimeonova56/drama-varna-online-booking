c8da8ca3ba135e7a43b16e127e6dd90bea762148
dianasim <diana.simeonova56@gmail.com>
2025-08-20 12:18:59 +0300

    add  angular materials spinenr & animate it along with play item to appear smoothly

--- src/app/shared/components/play-item/play-item.ts
+++ src/app/shared/components/play-item/play-item.ts
@@ -1,20 +1,28 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 import { Play } from '../../../models';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { PlayDateFormatPipe } from '../../pipes'
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 
 @Component({
   selector: 'app-play-item',
-  imports: [CommonModule, RouterModule, PlayDateFormatPipe],
+  imports: [CommonModule, RouterModule, PlayDateFormatPipe, MatProgressSpinnerModule],
   templateUrl: './play-item.html',
   styleUrl: './play-item.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PlayItem implements OnInit {
   @Input() play!: Play | null;
-  
+  loading = true;
 
-  ngOnInit(): void {
+  constructor(private cdr: ChangeDetectorRef) { }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.loading = false;
+      this.cdr.detectChanges();
+    }, 1000);
   }
 }
